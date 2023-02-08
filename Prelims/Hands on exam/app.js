66b9ac5b714ecf2e7ead5fd4b2fd8de5c4325050
BlankuZero <114331690+BlankuZero@users.noreply.github.com>
2023-02-08 16:14:14 +0800

    2 files modified

---
+++ Prelims/Hands on exam/app.js
@@ -0,0 +1,11 @@
+var greet = require ('./message');
+
+console.log(greet.greet('Louise'));
+
+var congratulations = require ('./message');
+
+console.log(congratulations.congratulations('Louise'));
+
+var farewell = require ('./message');
+
+console.log(farewell.farewell('Louise'));
---
+++ Prelims/Hands on exam/message.js
@@ -0,0 +1,15 @@
+module.exports.greet = function (name) {
+
+  console.log('Hello ' + name)
+  };
+  
+  module.exports.congratulations = function (name) {
+  
+  console.log('Congratulations ' + name)
+  };
+  
+  module.exports.farewell = function (name){
+  
+  console.log('Farewell ' + name)
+  };
+  
